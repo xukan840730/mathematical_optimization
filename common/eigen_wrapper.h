e4823ebaab2f79d4a35b0f5c4eabd9ab3d25340e
Kan Xu <xukanxukan@gmail.com>
2016-08-13 16:23:53 -0700

    organize fires

--- common/eigen_wrapper.h
+++ common/eigen_wrapper.h
@@ -3,10 +3,28 @@
 #ifndef _EIGEN_WRAPPER_H_
 #define _EIGEN_WRAPPER_H_
 
+
 void ChangeEVector(EVector* inout, int numRows);
 void ChangeEMatrix(EMatrix* inout, int numRows);
 
+EMatrix MatrixFromRowIdx(const EMatrix& A, const EVector& rowArr);
+EMatrix MatrixFromColIdx(const EMatrix& A, const EVector& colArr);
 
+// get row indices array after removing rows 
+EVector RmRows(int numRows, const EVector& rmRow);
+EMatrix MatrixRmvRowIdx(const EMatrix& A, const EVector& rmRow);
+EVector VectorFromIdx(const EVector& a, const EVector& rowArr);
+EVector VectorRmvIdx(const EVector& A, const EVector& rmIdx);
+
+bool anyNonzero(const EMatrix& A, float eps);
+// find non zeros and store them in row and col index.
+void findNonzeros(const EMatrix& m, EVector* rowIdx, EVector* colIdx);
+EVector findZeroIdx(const EVector& a, float eps);
+EVector findNnzIdx(const EVector& a, float eps);
+EVector colon(int j, int k); // matlab : operator
+EVector VecAppend(const EVector& a, const EVector& b);
+
+// eigen operation
 bool EigenLlt(const EMatrix& m, EMatrix* l);
 
 void EigenValVec(const EMatrix& m, 
